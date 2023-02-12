bd2c1431924d6ef9fb0f3951247d1f9c0d72002a
zinen <zinen@users.noreply.github.com>
2023-02-12 11:48:15 +0100

    Merging nodes into a info or cmd node

--- huawei-router/huawei-router.js
+++ huawei-router/huawei-router.js
@@ -4,6 +4,24 @@ module.exports = function (RED) {
   function HuaweiConfig(n) {
     RED.nodes.createNode(this, n)
     const node = this
+    node.promiseTimeout = function (delayMs) {
+      return new Promise(resolve => setTimeout(resolve, delayMs))
+    }
+    node.requestQueueing = async function (event) {
+      if (event == 'end') {
+        this.requestQueue--
+        return
+      } else if (event == 'wait') {
+        this.requestQueue++
+      } else {
+        throw new Error(`Error in requestQueueing handling. event=${event} should be wait or end`)
+      }
+      while (new Date().setSeconds() < this.requestQueueTimeout && this.requestQueue >= 3) {
+        await this.promiseTimeout(250)
+      }
+      this.requestQueueActive = true
+      this.requestQueueTimeout = new Date().setSeconds() + 10
+    }
     node.connect = async function () {
       try {
         this.now = (new Date()).getTime()
@@ -15,7 +33,7 @@ module.exports = function (RED) {
         this.sessionTimeout = n.sessionTimeout ? node.now + n.sessionTimeout * 1000 : node.now + 298000
         if (!this.connection) {
           // Make new session
-          this.connection = new huaweiLteApi.Connection(`http://${n.user}:${n.pass}@${n.url}`)
+          this.connection = new huaweiLteApi.Connection(`http://${node.credentials.user}:${node.credentials.pass}@${n.url}`)
           await this.connection.ready
         }
         return this.connection
@@ -28,7 +46,8 @@ module.exports = function (RED) {
       try {
         if ((new Date()).getTime() < node.sessionTimeout) {
           const user = new huaweiLteApi.User(await this.connect())
-          await user.logout()
+          // await user.logout()
+          Promise.race([user.logout(), node.promiseTimeout(7000)])
         }
       } finally {
         if (removed) {
@@ -42,20 +61,99 @@ module.exports = function (RED) {
       done()
     })
   }
-  RED.nodes.registerType('huawei-config', HuaweiConfig)
+  RED.nodes.registerType('huawei-config', HuaweiConfig, {
+    credentials: {
+      user: { type: "text" },
+      pass: { type: "password" }
+    }
+  })
 
-  function HuaweiLanHosts(config) {
+  function HuaweiCommand(config) {
     RED.nodes.createNode(this, config)
     const node = this
     node.on('input', async function (msg, send, done) {
-      node.server = RED.nodes.getNode(config.server)
-      node.status({ text: 'Connecting' })
+      const cmdOptions = ['MobileData', 'Reboot', 'SMSSend']
+      if (!cmdOptions.includes(config.cmdOption)) {
+        done(`Unknown command option ${config.cmdOption}`)
+        return
+      }
       try {
-        const lan = new huaweiLteApi.Lan(await node.server.connect())
-        const result = await lan.hostInfo()
-        msg.payload = result.Hosts.Host
+        node.server = RED.nodes.getNode(config.server)
+        node.status({ text: 'Connecting' })
+        await node.server.requestQueueing('wait')
+        switch (config.cmdOption) {
+          case cmdOptions[0]:
+            const allowedModes = ['on', 1, true, 'off', 0, false, 'toggle', 'off-on']
+            if (typeof (msg.mode) === 'undefined') {
+              msg.mode = node.mode
+            } else if (typeof (msg.mode) === 'string') { msg.mode = msg.mode.toLowerCase() }
+            if (!allowedModes.includes(msg.mode)) {
+              done('Input mode is not understood')
+              node.server.requestQueueing('end')
+              return
+            }
+            const dialUp = new huaweiLteApi.DialUp(await node.server.connect())
+            if (msg.mode === 'on' || msg.mode === 1 || msg.mode === true) {
+              msg.payload = await dialUp.setMobileDataswitch(1)
+              msg.state = msg.payload === 'OK' ? 1 : 0
+              send(msg)
+            } else if (msg.mode === 'off' || msg.mode === 0 || msg.mode === false) {
+              msg.payload = await dialUp.setMobileDataswitch(0)
+              msg.state = msg.payload === 'OK' ? 0 : 1
+              send(msg)
+            } else if (msg.mode === 'toggle') {
+              const { dataswitch } = await dialUp.mobileDataswitch()
+              msg.payload = await dialUp.setMobileDataswitch(1 - Number(dataswitch))
+              msg.state = msg.payload === 'OK' ? 1 - Number(dataswitch) : Number(dataswitch)
+              send(msg)
+            } else if (msg.mode === 'off-on') {
+              await dialUp.setMobileDataswitch(0)
+              msg.payload = await dialUp.setMobileDataswitch(1)
+              msg.state = msg.payload === 'OK' ? 1 : 0
+              send(msg)
+            }
+            break
+          case cmdOptions[1]:
+            const device = new huaweiLteApi.Device(await node.server.connect())
+            msg.payload = await device.reboot()
+            send(msg)
+            break
+          case cmdOptions[2]:
+            function parseNumber(inputNumber) {
+              if (typeof inputNumber === 'string') {
+                return [inputNumber]
+              } else if (typeof inputNumber === 'number') {
+                return [inputNumber.toString()]
+              } else if (Array.isArray(inputNumber)) {
+                return inputNumber.map(element => {
+                  return parseNumber(element)[0]
+                })
+              }
+              return ['']
+            }
+            msg.payload = String(msg.payload)
+            msg.number = parseNumber(msg.number || node.phoneNumber)
+            if (msg.number === ['']) {
+              done('Error understanding input number. Accepts text, number and array of text/numbers but not ' + String(msg.number || node.phoneNumber))
+              return
+            }
+            const SMS = new huaweiLteApi.Sms(await node.server.connect())
+            msg.payload = await SMS.sendSms(msg.number, msg.payload)
+            if (msg.payload !== 'OK') {
+              node.status({ fill: 'red', text: msg.payload })
+              done(msg.payload)
+              node.server.requestQueueing('end')
+              return
+            }
+            send(msg)
+            break
+          default:
+            done(`Unhandled command ${config.cmdOption}`)
+            node.server.requestQueueing('end')
+            return
+        }
+        node.server.requestQueueing('end')
         node.status({ text: '' })
-        send(msg)
         done()
       } catch (error) {
         if (error.code === 125002 || error.code === 125003) {
@@ -64,238 +162,60 @@ module.exports = function (RED) {
         }
         node.status({ fill: 'red', text: error.message })
         done(error.message)
+        node.server.requestQueueing('end')
       }
     })
   }
-  RED.nodes.registerType('huawei-lanhosts', HuaweiLanHosts)
+  RED.nodes.registerType('huawei-send-cmd', HuaweiCommand)
 
-  function HuaweiMobileData(config) {
-    RED.nodes.createNode(this, config)
-    const node = this
-    node.mode = config.mode
-    node.on('input', async function (msg, send, done) {
-      const allowedModes = ['on', 1, true, 'off', 0, false, 'toggle', 'off-on']
-      if (typeof (msg.mode) === 'undefined') {
-        msg.mode = node.mode
-      } else if (typeof (msg.mode) === 'string') { msg.mode = msg.mode.toLowerCase() }
-      if (!allowedModes.includes(msg.mode)) {
-        done('Input mode is not understood')
-        return
-      }
-      node.server = RED.nodes.getNode(config.server)
-      node.status({ text: 'Connecting' })
-      try {
-        const dialUp = new huaweiLteApi.DialUp(await node.server.connect())
-        if (msg.mode === 'on' || msg.mode === 1 || msg.mode === true) {
-          const result = await dialUp.setMobileDataswitch(1)
-          msg.payload = result
-          msg.state = result === 'OK' ? 1 : 0
-          send(msg)
-        } else if (msg.mode === 'off' || msg.mode === 0 || msg.mode === false) {
-          const result = await dialUp.setMobileDataswitch(0)
-          msg.payload = result
-          msg.state = result === 'OK' ? 0 : 1
-          send(msg)
-        } else if (msg.mode === 'toggle') {
-          const { dataswitch } = await dialUp.mobileDataswitch()
-          const result = await dialUp.setMobileDataswitch(1 - Number(dataswitch))
-          msg.payload = result
-          msg.state = result === 'OK' ? 1 - Number(dataswitch) : Number(dataswitch)
-          send(msg)
-        } else if (msg.mode === 'off-on') {
-          await dialUp.setMobileDataswitch(0)
-          const result = await dialUp.setMobileDataswitch(1)
-          msg.payload = result
-          msg.state = result === 'OK' ? 1 : 0
-          send(msg)
-        }
-      } catch (error) {
-        if (error.code === 125002 || error.code === 125003) {
-          // Make sure the session timeout happens on session error (125002/125003)
-          node.server.sessionTimeout = 0
-        }
-        node.status({ fill: 'red', text: error.message })
-        done(error.message)
-        return
-      }
-      node.status({ text: '' })
-      done()
-    })
-  }
-  RED.nodes.registerType('huawei-mobiledata', HuaweiMobileData)
-
-  function HuaweiSignal(config) {
-    RED.nodes.createNode(this, config)
-    const node = this
-    node.on('input', async function (msg, send, done) {
-      node.server = RED.nodes.getNode(config.server)
-      node.status({ text: 'Connecting' })
-      try {
-        const device = new huaweiLteApi.Device(await node.server.connect())
-        const result = await device.signal()
-        msg.payload = result
-        node.status({ text: '' })
-        send(msg)
-        done()
-      } catch (error) {
-        if (error.code === 125002 || error.code === 125003) {
-          // Make sure the session timeout happens on session error (125002/125003)
-          node.server.sessionTimeout = 0
-        }
-        node.status({ fill: 'red', text: error.message })
-        done(error.message)
-      }
-    })
-  }
-  RED.nodes.registerType('huawei-signal', HuaweiSignal)
-
-  function HuaweiWlanHosts(config) {
-    RED.nodes.createNode(this, config)
-    const node = this
-    node.on('input', async function (msg, send, done) {
-      node.server = RED.nodes.getNode(config.server)
-      node.status({ text: 'Connecting' })
-      try {
-        const device = new huaweiLteApi.WLan(await node.server.connect())
-        const result = await device.hostList()
-        msg.payload = result
-        node.status({ text: '' })
-        send(msg)
-        done()
-      } catch (error) {
-        if (error.code === 125002 || error.code === 125003) {
-          // Make sure the session timeout happens on session error (125002/125003)
-          node.server.sessionTimeout = 0
-        }
-        node.status({ fill: 'red', text: error.message })
-        done(error.message)
-      }
-    })
-  }
-  RED.nodes.registerType('huawei-wlanhosts', HuaweiWlanHosts)
-
-  function HuaweiReboot(config) {
-    RED.nodes.createNode(this, config)
-    const node = this
-    node.on('input', async function (msg, send, done) {
-      node.server = RED.nodes.getNode(config.server)
-      node.status({ text: 'Connecting' })
-      try {
-        const device = new huaweiLteApi.Device(await node.server.connect())
-        const result = await device.reboot()
-        msg.payload = result
-        node.status({ text: '' })
-        send(msg)
-        done()
-      } catch (error) {
-        if (error.code === 125002 || error.code === 125003) {
-          // Make sure the session timeout happens on session error (125002/125003)
-          node.server.sessionTimeout = 0
-        }
-        node.status({ fill: 'red', text: error.message })
-        done(error.message)
-      }
-    })
-  }
-  RED.nodes.registerType('huawei-reboot', HuaweiReboot)
-  function HuaweiSMSSend(config) {
-    RED.nodes.createNode(this, config)
-    const node = this
-    node.phoneNumber = config.phoneNumber
-    node.on('input', async function (msg, send, done) {
-      function parseNumber(inputNumber) {
-        if (typeof inputNumber === 'string') {
-          return [inputNumber]
-        } else if (typeof inputNumber === 'number') {
-          return [inputNumber.toString()]
-        } else if (Array.isArray(inputNumber)) {
-          return inputNumber.map(element => {
-            return parseNumber(element)[0]
-          })
-        }
-        return ['']
-      }
-      msg.payload = String(msg.payload)
-      msg.number = parseNumber(msg.number || node.phoneNumber)
-      if (msg.number === ['']) {
-        done('Error understanding input number. Accepts text, number and array of text/numbers but not ' + String(msg.number || node.phoneNumber))
-        return
-      }
-      node.server = RED.nodes.getNode(config.server)
-      node.status({ text: 'Connecting' })
-      try {
-        const SMS = new huaweiLteApi.Sms(await node.server.connect())
-        const result = await SMS.sendSms(msg.number, msg.payload)
-        if (result !== 'OK') {
-          node.status({ fill: 'red', text: result })
-          done(result)
-          return
-        }
-        send(msg)
-      } catch (error) {
-        if (error.code === 125002) {
-          // Make sure the session timeout happens on session error (125002)
-          node.server.sessionTimeout = 0
-        } else {
-          console.trace(error)
-        }
-        node.status({ fill: 'red', text: error.message })
-        done(error.message)
-        return
-      }
-      node.status({ text: '' })
-      done()
-    })
-  }
-  RED.nodes.registerType('huawei-sms-send', HuaweiSMSSend)
   function HuaweiInfo(config) {
     RED.nodes.createNode(this, config)
     const node = this
-    node.infoOption = config.infoOption
     node.on('input', async function (msg, send, done) {
       const infoOptions = ['Monitor-status', 'convergedStatus', 'checkNotifications', 'trafficStatistics', 'startDate', 'monthStatistics', 'Lan-hostInfo', 'WLan-hostInfo', 'signal']
-      if (!infoOptions.includes(node.infoOption)) {
-        done(`Unknown info option ${node.infoOption}`)
+      if (!infoOptions.includes(config.infoOption)) {
+        done(`Unknown info option ${config.infoOption}`)
         return
       }
       node.server = RED.nodes.getNode(config.server)
       node.status({ text: 'Connecting' })
+      await node.server.requestQueueing('wait')
       try {
-        let result = ''
-        switch (node.infoOption) {
+        switch (config.infoOption) {
           case infoOptions[0]:
-            result = await new huaweiLteApi.Monitoring(await node.server.connect()).status()
+            msg.payload = await new huaweiLteApi.Monitoring(await node.server.connect()).status()
             break
           case infoOptions[1]:
-            result = await new huaweiLteApi.Monitoring(await node.server.connect()).convergedStatus()
+            msg.payload = await new huaweiLteApi.Monitoring(await node.server.connect()).convergedStatus()
             break
           case infoOptions[2]:
-            result = await new huaweiLteApi.Monitoring(await node.server.connect()).checkNotifications()
+            msg.payload = await new huaweiLteApi.Monitoring(await node.server.connect()).checkNotifications()
             break
           case infoOptions[3]:
-            result = await new huaweiLteApi.Monitoring(await node.server.connect()).trafficStatistics()
+            msg.payload = await new huaweiLteApi.Monitoring(await node.server.connect()).trafficStatistics()
             break
           case infoOptions[4]:
-            result = await new huaweiLteApi.Monitoring(await node.server.connect()).startDate()
+            msg.payload = await new huaweiLteApi.Monitoring(await node.server.connect()).startDate()
             break
           case infoOptions[5]:
-            result = await new huaweiLteApi.Monitoring(await node.server.connect()).monthStatistics()
+            msg.payload = await new huaweiLteApi.Monitoring(await node.server.connect()).monthStatistics()
             break
           case infoOptions[6]:
-            result = await new huaweiLteApi.Lan(await node.server.connect()).hostInfo()
-            result = result.Hosts.Host
+            msg.payload = await new huaweiLteApi.Lan(await node.server.connect()).hostInfo()
+            msg.payload = msg.payload.Hosts.Host
             break
           case infoOptions[7]:
-            result = await new huaweiLteApi.WLan(await node.server.connect()).hostList()
+            msg.payload = await new huaweiLteApi.WLan(await node.server.connect()).hostList()
             break
           case infoOptions[8]:
-            result = await new huaweiLteApi.Device(await node.server.connect()).signal()
+            msg.payload = await new huaweiLteApi.Device(await node.server.connect()).signal()
             break
           default:
-            done('Switch case should not end here huawei-info')
+            done(`Unhandled info option ${config.cmdOption}`)
+            node.server.requestQueueing('end')
             return
         }
-        msg.payload = result
+        node.server.requestQueueing('end')
         node.status({ text: '' })
         send(msg)
         done()
@@ -306,6 +226,7 @@ module.exports = function (RED) {
         }
         node.status({ fill: 'red', text: error.message })
         done(error.message)
+        node.server.requestQueueing('end')
       }
     })
   }
